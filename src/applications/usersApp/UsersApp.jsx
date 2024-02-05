a783c3c72b196fa6069c441c1145a347110c1e01
Azim <azimarynaliev@icloud.com>
2024-02-05 11:15:30 -0600

    new crud project

---
+++ src/applications/usersApp/UsersApp.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import UsersTable from "../../components/usersTable/UsersTable";
+
+const UsersApp = () => {
+  return (
+    <div>
+      <UsersTable />
+    </div>
+  );
+};
+
+export default UsersApp;
