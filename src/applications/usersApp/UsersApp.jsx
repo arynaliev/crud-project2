b4bcf008e3356dac287337c20800522365f482b5
Azim <azimarynaliev@icloud.com>
2024-02-05 11:42:01 -0600

    table row created

--- src/applications/usersApp/UsersApp.jsx
+++ src/applications/usersApp/UsersApp.jsx
@@ -1,11 +1,29 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import "./usersApp.style.css";
 import UsersTable from "../../components/usersTable/UsersTable";
 
 const UsersApp = () => {
+  const [usersList, setUsersList] = useState([]);
+
+  const getUsers = async () => {
+    try {
+      const response = await fetch(
+        "https://jsonplaceholder.typicode.com/users"
+      );
+      const data = response.json();
+      setUsersList(data);
+    } catch (err) {
+      console.log("error:", err);
+    }
+  };
+
+  useEffect(() => {
+    getUsers();
+  }, []);
   return (
-    <div>
-      <UsersTable />
-    </div>
+    <>
+      <UsersTable usersList={usersList} />
+    </>
   );
 };
 
--- src/components/usersTable/UsersTable.jsx
+++ src/components/usersTable/UsersTable.jsx
@@ -1,7 +1,22 @@
 import React from "react";
+import "./usersTable.style.css";
 
-const UsersTable = () => {
-  return <div>UsersTable</div>;
+const UsersTable = ({ usersList }) => {
+  return (
+    <div className="table-container">
+      <table>
+        <thead>
+          <tr>
+            <td>ID</td>
+            <td>Name</td>
+            <td>Email</td>
+            <td>Phone</td>
+            <td>Website</td>
+          </tr>
+        </thead>
+      </table>
+    </div>
+  );
 };
 
 export default UsersTable;
